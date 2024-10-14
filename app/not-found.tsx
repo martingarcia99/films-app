88fcd411b55c7f10c28b15ef535b582890aaf996
Martin Jose Garcia Muñoz <martin.garcia-munoz@stud.uni-due.de>
2024-10-14 17:42:06 +0200

    fix vercel deploy
    fix vercel deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,11 @@
 'use client'
- 
-import Link from 'next/link'
+
 import { useEffect } from 'react'
  
 export default function Error({
   error,
-  reset,
 }: {
   error: Error & { digest?: string }
-  reset: () => void
 }) {
   useEffect(() => {
     console.error(error)
